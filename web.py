522eda9aa5f0198d8ae29e8ae51cfdd346cdf1aa
amine ziad ounnoughene <86957029+amine-ziad-ounnoughene@users.noreply.github.com>
2021-11-01 18:45:33 +0100

    Update web.py

--- web.py
+++ web.py
@@ -1,7 +1,7 @@
 import streamlit as st
 st.title("**Devos**")
 st.subheader("Devos is an instagram page that shares data science , programming , tech news and courses and more")
-st.header("Links")
+st.title("Links")
 st.subheader("Post :  first step to learn programming")
 st.write(" [crash course computer science](https://www.youtube.com/playlist?list=PL8dPuuaLjXtNlUrzyH5r6jN9ulIgZBpdo)")
 st.video('https://www.youtube.com/playlist?list=PL8dPuuaLjXtNlUrzyH5r6jN9ulIgZBpdo')
@@ -9,7 +9,7 @@ st.subheader("Post :  second step to learn programming ")
 st.write("[python course in arabic](https://www.edraak.org/course/course-v1:PSUT+PL101+2018_SP/)")
 st.write("[python coursera university of michigen](https://www.coursera.org/specializations/python)")
 st.write("[free code camp python course in 4 hours](https://www.youtube.com/watch?v=rfscVS0vtbw&t=8137s)")
-st.header('data viz : top 5 graphs in plotly')
+st.subheader('data viz : top 5 graphs in plotly')
 st.write("jupyter notebook [github](https://github.com/amine-ziad-ounnoughene/devos/blob/main/graphs.ipynb)")
 st.code("""## imshow
 
